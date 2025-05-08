85d73d5953a2c3a2e9dc5da507e3bcee100dcc81
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-05-08 18:32:02 -0400

    Initialize schemas.sql for database structure

---
+++ src/main/resources/schemas.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS currency_rate;
+CREATE DATABASE currency_rate;
+
+USE currency_rate;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT NOT NULL,
+                       username VARCHAR(25) NOT NULL,
+                       `password` VARCHAR(25) NOT NULL,
+                       email VARCHAR(255) NOT NULL,
+                       CONSTRAINT pk_user PRIMARY KEY (id),
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS exchange_alerts;
+CREATE TABLE exchange_alert (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                user_email VARCHAR(255) NOT NULL,
+                                base_currency VARCHAR(10) NOT NULL,
+                                target_currency VARCHAR(10) NOT NULL,
+                                target_rate DECIMAL(10,4) NOT NULL,
+                                notified BOOLEAN DEFAULT FALSE
+);
