b652a83a860621661e69d401542502f44a75579b
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-05-08 18:32:47 -0400

    Implement all data mappers

---
+++ src/main/java/org/sandyf/currencyexchangealert/dao/mappers/ExchangeAlertRowMapper.java
@@ -0,0 +1,22 @@
+package org.sandyf.currencyexchangealert.dao.mappers;
+
+import org.sandyf.currencyexchangealert.model.ExchangeAlert;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ExchangeAlertRowMapper implements RowMapper<ExchangeAlert> {
+
+    @Override
+    public ExchangeAlert mapRow(ResultSet rs, int rowNum) throws SQLException {
+        ExchangeAlert alert = new ExchangeAlert();
+        alert.setId(rs.getInt("id"));
+        alert.setUserEmail(rs.getString("user_email"));
+        alert.setBaseCurrency(rs.getString("base_currency"));
+        alert.setTargetCurrency(rs.getString("target_currency"));
+        alert.setTargetRate(rs.getBigDecimal("target_rate"));
+        alert.setNotified(rs.getBoolean("notified"));
+        return alert;
+    }
+}
---
+++ src/main/java/org/sandyf/currencyexchangealert/dao/mappers/UserRowMapper.java
@@ -0,0 +1,20 @@
+package org.sandyf.currencyexchangealert.dao.mappers;
+
+import org.sandyf.currencyexchangealert.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(rs.getInt("id"));
+        user.setUsername(rs.getString("username"));
+        user.setEmail(rs.getString("email"));
+        user.setPassword(rs.getString("password"));
+        user.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+        return user;
+    }
+}
