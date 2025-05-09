122200a707b7e52d4b44387de7364d6d2a9b3d10
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-05-08 22:30:17 -0400

    Update CurrencyExchangeAlertApplication.java

--- src/main/java/org/sandyf/currencyexchangealert/CurrencyExchangeAlertApplication.java
+++ src/main/java/org/sandyf/currencyexchangealert/CurrencyExchangeAlertApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+    }
+)
 @EnableScheduling
 public class CurrencyExchangeAlertApplication {
 
